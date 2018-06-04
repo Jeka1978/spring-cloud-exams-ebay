d498508da9dcb7e8b2c5fde0d706e5af983482dd
bsevgeny@gmail.com <papakita2009>
2018-06-05 01:17:31 +0300

    lookup with DiscoveryClient

--- mathematic/src/main/java/com/ebay/exams/MathApp.java
+++ mathematic/src/main/java/com/ebay/exams/MathApp.java
@@ -2,6 +2,7 @@ package com.ebay.exams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
  * @author Evgeny Borisov
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class MathApp {
 
 
